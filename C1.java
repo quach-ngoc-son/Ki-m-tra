30df071734991693c3f4da72db34f754d545c9b4
quach-ngoc-son <99794494+quach-ngoc-son@users.noreply.github.com>
2022-05-11 17:43:06 +0700

    Update C1.java

--- C1.java
+++ C1.java
@@ -1,54 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package cau1;
-import java.util.*;
-/**
- *
- * @author ADMIN
- */
-public class Book {
-    private String title;
-    private double price;
-    public String getTitle(){
-        return title;
-    }
-    public void setTitle(String title){
-        this.title = title;
-    }
-    public double getPrice(){
-        return price;
-    }
-    public void setPrice(double price){
-        this.price = price;
-    }
-    public double getImportTax(){
-        double thue = this.price * 10 / 100;
-        return thue;
-    }
-    public void setBook(){
-        Scanner nhap= new Scanner(System.in);
-        System.out.println("Nhap ten sach: ");
-        title = nhap.nextLine();
-        System.out.println("Nhap gia bia: ");
-        price = nhap.nextDouble();
-    }
-    public void getBook(){
-        System.out.println("Ten sach: " + title);
-        System.out.println("Gia bia: "+ price);
-        System.out.println("Thue nhap khau:" + getImportTax());
-    }
-     public static void main(String[] args) {
-        Book a= new Book();
-        Scanner nhap= new Scanner(System.in);
-        System.out.println("Nhập vào số sách: ");
-        int n = nhap.nextInt();
-        for(int i=0; i<n; i++){
-            a.setBook();
-        }
-        for(int i=0; i<n; i++){
-            a.getBook();
-        }
-    }
+package quachngocson_2021050572;
+
+import java.util.Scanner;
+
+public class book {
+private String title;
+private int price,VAT;
+public void getlmportVAT(){
+VAT=(price*5)/100; 
+}
+void setbook(){
+Scanner sc = new Scanner(System.in);
+System.out.println("Nhap ten sach: ");
+        title = sc.nextLine();
+System.out.println("Nhap gia bia: ");
+        price = sc.nextInt();
+}  
+void getbook(){
+    System.out.println("Ten sach:"+title+"\nGia bia"+price+"\nthue VAT"+VAT);
+}
 }
